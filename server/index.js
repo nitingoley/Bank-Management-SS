4f90fd2540b02a655b8989fe5004dcdc9f5ba0bb
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2025-07-20 16:02:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://bank-management-ss.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
